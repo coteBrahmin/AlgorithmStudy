fd76c13eab7343a4151e7798a55e60aa095c819d
loveyrooney <loveyrooney@gmail.com>
2023-05-30 14:19:50 +0900

    [BOJ-lv2]오븐+주사위세개_kim

---
+++ Baekjoon/lv2/2480/주사위세개_kim.py
@@ -0,0 +1,11 @@
+A,B,C = map(int,input().split())
+#세 수가 모두 같은 경우
+if A == B and B == C: print(10000+1000*A)
+#두 수가 같은 경우
+elif A == B or A == C: print(1000+100*A)
+elif B == C: print(1000+100*B)
+#모두 다른 경우
+else:
+    numlist = [A,B,C]
+    numlist.sort()
+    print(100*numlist[-1])
