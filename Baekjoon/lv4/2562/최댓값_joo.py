13fa80f6ffb35d971c435b94057e99cab0ab51bc
joosomi <wnthal1211@naver.com>
2023-06-01 18:52:42 +0900

    [BOJ-lv4]7문제_joo

---
+++ Baekjoon/lv4/2562/최댓값_joo.py
@@ -0,0 +1,16 @@
+import sys
+
+#처음엔 리스트로 하다가, 딕셔너리 방법을 시도하였는데
+#딕셔너리는 키-값 순서를 잘 활용하는 게 중요한 것 같습니다.
+
+nums = {0: i for i in range(1, 10)}
+
+#그냥 0~9째 줄을 key 값, input 값을 value로 넣어주고 최댓값을 뽑아줍니다.
+# {0: 9, 3: 1, 29: 2, 38: 3, 12: 4, 57: 5, 74: 6, 40: 7, 85: 8} 이런 형태가 됩니다.
+for i in range(1, 10):
+    num = int(sys.stdin.readline())
+    nums[num] = i
+
+max = max(nums.keys())
+print(max)
+print(nums[max])
