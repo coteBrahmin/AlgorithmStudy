d992d5fa926a9c8df07476f65df390d58688f922
Somi Joo <109468380+joosomi@users.noreply.github.com>
2023-06-01 19:06:25 +0900

    Update 최댓값_joo.py

--- Baekjoon/lv4/2562/최댓값_joo.py
+++ Baekjoon/lv4/2562/최댓값_joo.py
@@ -5,7 +5,7 @@ import sys
 
 nums = {0: i for i in range(1, 10)}
 
-#그냥 0~9째 줄을 key 값, input 값을 value로 넣어주고 최댓값을 뽑아줍니다.
+#몇번 째 줄인지를 value, input 값을 key로 넣어주고 최댓값을 뽑아줍니다.
 # {0: 9, 3: 1, 29: 2, 38: 3, 12: 4, 57: 5, 74: 6, 40: 7, 85: 8} 이런 형태가 됩니다.
 for i in range(1, 10):
     num = int(sys.stdin.readline())
